8f38983b3e87606c3c5541e3becd5c2d9f53d27f
Huuwx <chumanhhuu2@gmail.com>
2024-11-01 18:38:56 +0700

    add Seal enemy and Shark enemy

--- Assets/Scripts/Obstacle/ObstacleController.cs
+++ Assets/Scripts/Obstacle/ObstacleController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    protected Rigidbody2D rb;
 
     public Rigidbody2D getRb() { return rb; }
 
-    private void Start()
+    protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    protected virtual void Update()
     {
         HandleSpeed();
         DestroyObstacle();
---
+++ Assets/Scripts/Obstacle/SharkController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkController : ObstacleController
+{
+    public float posToJump = -4f;
+    public float jumpPoint = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (transform.position.x <= jumpPoint)
+        {
+            Debug.Log("ok");
+            StartCoroutine(WaterJump());
+        }
+    }
+
+    private IEnumerator WaterJump()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(transform.position.x, posToJump, transform.position.z);
+
+        float t = 0;
+
+        while (t <= 1)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            t += Time.deltaTime * 4;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = endPos;
+
+    }
+}
